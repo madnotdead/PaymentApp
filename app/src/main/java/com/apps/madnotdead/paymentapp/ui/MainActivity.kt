a433cd1b5f08d7159454dfd44400940b49b9f37e
madnotdead <leandromaguna@gmail.com>
2017-06-17 13:07:19 -0300

    Refactor

--- app/src/main/java/com/apps/madnotdead/paymentapp/ui/MainActivity.kt
+++ app/src/main/java/com/apps/madnotdead/paymentapp/ui/MainActivity.kt
@@ -1,17 +1,9 @@
 package com.apps.madnotdead.paymentapp.ui
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.support.v7.app.AppCompatActivity
 import com.apps.madnotdead.paymentapp.R
-import com.apps.madnotdead.paymentapp.data.model.PaymentMethod
 import com.apps.madnotdead.paymentapp.data.remote.RestApi
-import rx.Observable
-import rx.Scheduler
-import rx.android.schedulers.AndroidSchedulers
-import rx.functions.Action1
-import rx.schedulers.Schedulers
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,16 +15,6 @@ class MainActivity : AppCompatActivity() {
         var api = RestApi()
 
         var paymentMethodsList = api.getPaymentMethods("444a9ef5-8a6b-429f-abdf-587639155d88")
-
-
-
-        api.getPaymentMethods("444a9ef5-8a6b-429f-abdf-587639155d88")
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(onNext {
-
-                })
-
 
     }
 }
